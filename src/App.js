6f9e72895f4aeaaeea5a4f5a508e2f123e95ad88
lusaduma mata <mlusaduma@gmail.com>
2024-02-06 15:54:34 +0200

    added navigation for react portfolio

--- src/App.js
+++ src/App.js
@@ -1,24 +1,63 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+
+      <nav class="navbar">
+        <ul class="menu" id="menu">
+          <li><a class ="active" href ="#"> Home</a></li>
+          <li><a href ="#about_me">About Me</a></li>
+          <li><a href ="#education">Education</a></li>
+          <li><a href ="#work_experience">Work Experience</a></li>
+          <li><a href ="#projects_completed">Projects</a></li>
+          <li><a href ="#contact_me"> Contact Me</a></li>    
+        </ul>
+
+        
+        <div class="menu-icon" id="menu-icon">
+            <div class="bar"></div>
+            <div class="bar"></div>
+            <div class="bar"></div>
+        </div>
+      </nav>
+
+
+      <div class="center">
+        <div class="home-container">
+            <section id="lusaduma-mata">
+                <h1 class="heading"><span class="text autoplay"> LUSADUMA MATA </span></h1>
+            </section>
+
+            <h3> Junior Software Developer </h3>
+            
+           
+            
+            
+
+            <div class="social-icons">
+                <a href="https://github.com/Lusa-mata?tab=repositories" target="_blank"><i class="fab fa-github"></i></a>
+                <a href="https://www.linkedin.com/in/lusaduma-ok-mata/" target="_blank"><i class="fab fa-linkedin"></i></a>
+            </div>
+            
+
+            
+            <div class="button-container">
+                <a href= "CV/Lusaduma Mata CV Updated.docx" class="download-button" download>Download my CV</a>
+            </div>
+
+           
+    
+        </div>
     </div>
+
+
+
+
+    </div>
+   
   );
 }
 
