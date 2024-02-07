a14db2a03205b37c9b2e3af15a2090f6d61d7787
lusaduma mata <mlusaduma@gmail.com>
2024-02-07 10:33:04 +0200

    updated

--- src/App.js
+++ src/App.js
@@ -5,58 +5,60 @@ function App() {
   return (
     <div className="App">
 
+      <nav className="navbar">
+        <img 
+          src="images/Me-removebg-preview.png" 
+          alt="Lusaduma" 
+        />
 
-      <nav class="navbar">
-        <ul class="menu" id="menu">
-          <li><a class ="active" href ="#"> Home</a></li>
-          <li><a href ="#about_me">About Me</a></li>
-          <li><a href ="#work_experience">Work Experience</a></li>
-          <li><a href ="#projects_completed">Projects</a></li>
-          <li><a href ="#contact_me"> Contact Me</a></li>    
+        <ul className="menu" id="menu">
+          <li><a className="active" href="#"> Home</a></li>
+          <li><a href="#about_me">About Me</a></li>
+          <li><a href="#work_experience">Work Experience</a></li>
+          <li><a href="#projects_completed">Projects</a></li>
+          <li><a href="#contact_me"> Contact Me</a></li>    
         </ul>
 
-        
-        <div class="menu-icon" id="menu-icon">
-            <div class="bar"></div>
-            <div class="bar"></div>
-            <div class="bar"></div>
+        <div className="menu-icon" id="menu-icon">
+          <div className="bar"></div>
+          <div className="bar"></div>
+          <div className="bar"></div>
         </div>
       </nav>
 
- 
-      <div class="center">
-        <div class="home-container">
-            <section id="lusaduma-mata">
-                <h1 class="heading"><span class="text autoplay"> LUSADUMA MATA </span></h1>
-            </section>
+      <div className="center">
+        <div className="home-container">
+          <section id="lusaduma-mata">
+            <h1 className="heading">
+              <span className="text autoplay"> LUSADUMA MATA </span>
+            </h1>
+          </section>
 
-            <h3> Junior Software Developer </h3>
-            
-           
-            
-             
+          <h3>Junior Software Developer</h3>
+          <br />
 
-            <div class="social-icons">
-                <a href="https://github.com/Lusa-mata?tab=repositories" target="_blank"><i class="fab fa-github"></i></a>
-                <a href="https://www.linkedin.com/in/lusaduma-ok-mata/" target="_blank"><i class="fab fa-linkedin"></i></a>
-            </div>
-            
+          <div className="profile-img">
+            <img src={require('./images/graduation 2.jfif')} alt="Lusaduma Graduation" />
+          </div>
+          <br />
 
-            
-            <div class="button-container">
-                <a href= "CV/Lusaduma Mata CV Updated.docx" class="download-button" download>Download my CV</a>
-            </div>
+          <br />
 
-           
-    
+          <div className="button-container">
+            <a
+              href="CV/Lusaduma Mata CV Updated.docx"
+              className="download-button"
+              download
+            >
+              Download my CV
+            </a>
+          </div>
+
+          <br />
+          <br />
         </div>
+      </div>
     </div>
-
-
-
-
-    </div>
-   
   );
 }
 
