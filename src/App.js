d5882528d4d100932f2d6dd54380fb9ca8216ac0
lusaduma mata <mlusaduma@gmail.com>
2024-02-09 10:12:46 +0200

    prettified code

--- src/App.js
+++ src/App.js
@@ -41,7 +41,6 @@ function App() {
             <img src={require('./images/graduation 2.jfif')} alt="Lusaduma Graduation" height={"450"} />
           </div>
           <br />
-
           <br />
 
           <div className="button-container">
@@ -65,17 +64,15 @@ function App() {
 
 
       <div className="center">
-      <br />
           <br />
-    
-      <section id="about">
-            <h1 className="heading">
-              <span className="text autoplay"> ABOUT ME </span>
-            </h1>
-          </section>
-</div>
+          <br />
+        <section id="about">
+          <h1 className="heading">
+            <span className="text autoplay"> ABOUT ME </span>
+          </h1>
+        </section>
+      </div>
 
-<br />
           <br />
           <br />
           <br />
@@ -98,129 +95,130 @@ function App() {
           <br />
           <br />
           <br />
-    <div className="center">
-      <section id="work">
-            <h1 className="heading">
-              <span className="text autoplay"> WORK EXPERIENCE </span>
-            </h1>
-          </section>
-</div>
-    
-    
-<br />
           <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-    <div className="center">
-      <section id="projects">
-            <h1 className="heading">
-              <span className="text autoplay"> PROJECTS COMPLETED </span>
-            </h1>
-          </section>
-</div>
-    
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-    <div className="center">
-      <section id="contact">
-            <h1 className="heading">
-              <span className="text autoplay"> CONTACT ME </span>
-            </h1>
-          </section>
-</div>
+
+      <div className="center">
+        <section id="work">
+          <h1 className="heading">
+            <span className="text autoplay"> WORK EXPERIENCE </span>
+          </h1>
+        </section>
+      </div>
     
     
-
-<br />
           <br />
-    <div class="center">
-         
-            <div class="form-container">
-            <div className="form-image">
-              <img src={require('./images/contact me form pic.png')} alt="Image Description" />
-            </div>
-                <form class="form">
-                    
-                    <div class="input-group">
-                        <label for="username">Name and Surname</label>
-                        <input type="text" name="username" id="username" maxlength="30" required></input>
-                    </div>
-                    <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
 
-                    <div class="input-group">
-                        <label for="email">Email address</label>
-                        <input type="email" name="email" id="email" maxlength="50" required></input>
-                    </div>
-                    <br />
+      <div className="center">
+        <section id="projects">
+          <h1 className="heading">
+            <span className="text autoplay"> PROJECTS COMPLETED </span>
+          </h1>
+        </section>
+      </div>
+    
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
 
-                    <div class="input-group">
-                        <label for="submit">Subject line</label>
-                        <input type="text" name="subject" id="subject" maxlength="50" required></input>
-                    </div>
-                    <br />
+      <div className="center">
+        <section id="contact">
+          <h1 className="heading">
+            <span className="text autoplay"> CONTACT ME </span>
+          </h1>
+        </section>
+      </div>
+    
+          <br />
+          <br />
 
-                    <div class="input-group">
-                        <label for="text">Message</label>
-                        <textarea id="message" name="message" maxlength="250" rows="8" required></textarea>
-                    </div>
-                    <br />
-                    <br />
+      <div class="center">
+        <div class="form-container">
+          <div className="form-image">
+            <img src={require('./images/contact me form pic.png')} alt="Image Description" />
+          </div>
 
-                    <button class="submit">SUBMIT</button>
-                    <br />
-                    <input type="reset" class="clear" value="RESET"></input>
-                </form>
+          <form class="form">      
+            <div class="input-group">
+              <label for="username">Name and Surname</label>
+              <input type="text" name="username" id="username" maxlength="30" required></input>
             </div>
 
+            <br />
 
+            <div class="input-group">
+              <label for="email">Email address</label>
+              <input type="email" name="email" id="email" maxlength="50" required></input>
+            </div>
+
+            <br />
+
+            <div class="input-group">
+              <label for="submit">Subject line</label>
+              <input type="text" name="subject" id="subject" maxlength="50" required></input>
+            </div>
+
+            <br />
+
+            <div class="input-group">
+              <label for="text">Message</label>
+              <textarea id="message" name="message" maxlength="250" rows="8" required></textarea>
+            </div>
+          
+            <br />
+            <br />
+
+            <button class="submit">SUBMIT</button>
+            <br />
+          
+            <input type="reset" class="clear" value="RESET"></input>
             
-
-
-                    
-
+          </form>
         </div>
+      </div>
 
 
-        <br />
-                    <br />
+            <br />
+            <br />
 
       
                     
