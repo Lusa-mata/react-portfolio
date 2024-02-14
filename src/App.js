21fa57c8fc9c6e9d0488796125228ad8a069b5d1
lusaduma mata <mlusaduma@gmail.com>
2024-02-14 14:13:54 +0200

    added a toggle button

--- src/App.js
+++ src/App.js
@@ -73,15 +73,17 @@ function App() {
         {/* Overview card and picture of me */}
         <div className="card-Overview">
         <h2 style={{ textAlign: "center" }}>Overview</h2>
-          <img src={require('./images/Graduation_picture-removebg-preview.png')} height={"450"} />
-          <p style={{ textAlign: "center" }}> 
-            I graduated with a diploma in software development from IIE Rosebank College <br/> 
-            in 2022, where I gained a solid foundation in information technology and <br/>
-            software engineering. I also learned and applied various programming languages, <br/>
-            such as Java, C#, and SQL, in different software applications and projects. <br/><br/>
-            I am passionate about solving problems, resilient, and focused, and I aspire <br/>
-            to become a professional software developer who can create value and positive <br/>
-            change through technology. <br/><br/>
+        <br />
+        <br />
+          <img src={require('./images/Graduation_picture-removebg-preview.png')} />
+          <p> 
+            I graduated with a diploma in software development from IIE Rosebank College 
+            in 2022, where I gained a solid foundation in information technology and 
+            software engineering. I also learned and applied various programming languages, 
+            such as Java, C#, and SQL, in different software applications and projects. 
+            I am passionate about solving problems, resilient, and focused, and I aspire 
+            to become a professional software developer who can create value and positive 
+            change through technology. 
           </p>
         </div>
 
@@ -136,6 +138,10 @@ function App() {
         </div>
       </div>
 
+      <br></br>
+
+      
+
 
 
 
@@ -186,7 +192,7 @@ function App() {
           <div style={{ display: 'flex', justifyContent: 'center' }}>
             <div class="book">
               <p>Description of project 2 goes here</p>
-        
+              
               <div class="cover">
                 <img src={require('./images/old portfolio.png')} alt="Cake image" />
               </div>
@@ -340,6 +346,25 @@ function App() {
         <p class="second-text">Lorem Ipsum</p>
     </div>
   </div>*/}
+
+<div className="center">
+    {/* More projects */}
+      <div className="card-more">
+        VIEW MORE PROJECTS ON GITHUB
+
+        <input id="checkbox" type="checkbox" />
+<label class="switch" for="checkbox">
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" class="slider">
+    <path
+      d="M288 32c0-17.7-14.3-32-32-32s-32 14.3-32 32V256c0 17.7 14.3 32 32 32s32-14.3 32-32V32zM143.5 120.6c13.6-11.3 15.4-31.5 4.1-45.1s-31.5-15.4-45.1-4.1C49.7 115.4 16 181.8 16 256c0 132.5 107.5 240 240 240s240-107.5 240-240c0-74.2-33.8-140.6-86.6-184.6c-13.6-11.3-33.8-9.4-45.1 4.1s-9.4 33.8 4.1 45.1c38.9 32.3 63.5 81 63.5 135.4c0 97.2-78.8 176-176 176s-176-78.8-176-176c0-54.4 24.7-103.1 63.5-135.4z"
+    ></path>
+  </svg>
+</label>
+      </div>
+    </div>
+
+    
+
       
 
 
