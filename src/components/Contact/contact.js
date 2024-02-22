f3845dd94fdd56a508faceccb45b6b8125621cd1
lusaduma mata <mlusaduma@gmail.com>
2024-02-22 07:28:49 +0200

    updated reCAPTCHA

--- src/components/Contact/contact.js
+++ src/components/Contact/contact.js
@@ -59,13 +59,15 @@ function Contact() {
             <br />
             <br />
 
-            <div className="reCAPTCHA">
             {/* reCAPTCHA */}
+            <div className="reCAPTCHA">
             <ReCAPTCHA
-              sitekey="6Lcq53opAAAAAP3aommd_71NaaVf_4z2tHvWL52a" // Replace with your reCAPTCHA site key
+              sitekey="6Lcq53opAAAAAP3aommd_71NaaVf_4z2tHvWL52a" 
               onChange={(value) => console.log("reCAPTCHA value:", value)}
+              
             />
             </div>
+
 
             {/* submit and reset buttons below */}
             <button type="submit" className="submit">
